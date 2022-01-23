3bf55d13c572099d05020e27f247c21d87951838
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-23 12:21:42 +0900

    초기 커밋.

---
+++ NotePad/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace NotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.richTextBoxMemo = new System.Windows.Forms.RichTextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.comboBoxSearch = new System.Windows.Forms.ComboBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.checkBoxDelShow = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "메모";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave.Font = new System.Drawing.Font("맑은 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonSave.ForeColor = System.Drawing.Color.White;
+            this.buttonSave.Location = new System.Drawing.Point(40, 296);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(109, 62);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "저장";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(483, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "메모보기";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(485, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(594, 325);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonDel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonDel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDel.Font = new System.Drawing.Font("맑은 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonDel.ForeColor = System.Drawing.Color.White;
+            this.buttonDel.Location = new System.Drawing.Point(188, 296);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(109, 62);
+            this.buttonDel.TabIndex = 7;
+            this.buttonDel.Text = "삭제";
+            this.buttonDel.UseVisualStyleBackColor = false;
+            this.buttonDel.Click += new System.EventHandler(this.buttonDel_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUpdate.Font = new System.Drawing.Font("맑은 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonUpdate.ForeColor = System.Drawing.Color.White;
+            this.buttonUpdate.Location = new System.Drawing.Point(327, 296);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(109, 62);
+            this.buttonUpdate.TabIndex = 7;
+            this.buttonUpdate.Text = "수정";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "이름";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(40, 9);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 21);
+            this.textBoxName.TabIndex = 9;
+            // 
+            // richTextBoxMemo
+            // 
+            this.richTextBoxMemo.Location = new System.Drawing.Point(41, 37);
+            this.richTextBoxMemo.Name = "richTextBoxMemo";
+            this.richTextBoxMemo.Size = new System.Drawing.Size(423, 235);
+            this.richTextBoxMemo.TabIndex = 10;
+            this.richTextBoxMemo.Text = "";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonSearch.ForeColor = System.Drawing.Color.White;
+            this.buttonSearch.Location = new System.Drawing.Point(970, 4);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(109, 26);
+            this.buttonSearch.TabIndex = 11;
+            this.buttonSearch.Text = "검색";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // comboBoxSearch
+            // 
+            this.comboBoxSearch.FormattingEnabled = true;
+            this.comboBoxSearch.Items.AddRange(new object[] {
+            "이름",
+            "메모"});
+            this.comboBoxSearch.Location = new System.Drawing.Point(797, 7);
+            this.comboBoxSearch.Name = "comboBoxSearch";
+            this.comboBoxSearch.Size = new System.Drawing.Size(52, 20);
+            this.comboBoxSearch.TabIndex = 12;
+            this.comboBoxSearch.Text = "이름";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(864, 7);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(100, 21);
+            this.textBoxSearch.TabIndex = 13;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // checkBoxDelShow
+            // 
+            this.checkBoxDelShow.AutoSize = true;
+            this.checkBoxDelShow.Location = new System.Drawing.Point(542, 8);
+            this.checkBoxDelShow.Name = "checkBoxDelShow";
+            this.checkBoxDelShow.Size = new System.Drawing.Size(76, 16);
+            this.checkBoxDelShow.TabIndex = 14;
+            this.checkBoxDelShow.Text = "삭제 보기";
+            this.checkBoxDelShow.UseVisualStyleBackColor = true;
+            this.checkBoxDelShow.CheckedChanged += new System.EventHandler(this.checkBoxDelShow_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 380);
+            this.Controls.Add(this.checkBoxDelShow);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.comboBoxSearch);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.richTextBoxMemo);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "메모관리프로그램";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.RichTextBox richTextBoxMemo;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ComboBox comboBoxSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.CheckBox checkBoxDelShow;
+    }
+}
+
---
+++ NotePad/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace NotePad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string seqCount()
+        {
+            try
+            {
+                string Connect = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+                string Query = "SELECT MAX(noteSeq)+1 AS seqMax FROM dawoon.dc_notemanage;";
+                MySqlConnection con = new MySqlConnection(Connect);
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(Query, con);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    return rdr["seqMax"].ToString();
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return "";
+        }
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if(textBoxName.Text == "")
+            {
+                MessageBox.Show("이름을 입력해주세요");
+                textBoxName.Focus();
+                return;
+            }
+
+            try
+            {
+                string Connect = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+                string Query = "insert into dawoon.dc_notemanage(noteSeq,userNm,memo,flagYN,regDate,issueDate,issueID) values('"
+                    + seqCount() + "','" + textBoxName.Text.Trim() + "','" + richTextBoxMemo.Text.Trim() + "','Y',now(),now(),'CDY');";
+                MySqlConnection con = new MySqlConnection(Connect);
+                MySqlCommand Comm = new MySqlCommand(Query, con);
+                MySqlDataReader Read;
+                con.Open();
+                Read = Comm.ExecuteReader();
+                MessageBox.Show("저장완료");
+                con.Close();
+                textBoxName.Text = "";
+                textBoxSearch.Text = "";
+                richTextBoxMemo.Text = "";
+                buttonSearch_Click(sender, e);
+            }
+            catch (Exception ex)   
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonSearch_Click(sender, e);
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string Connect = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+            string Query = "select * from dawoon.dc_notemanage;";
+            string searchtext = textBoxSearch.Text.Trim();
+            string keyText = comboBoxSearch.Text;
+            string field = "";
+            if (keyText == "이름") field = "userNm";
+            else if (keyText == "메모") field = "memo";
+            string flagYN = "";
+            if (checkBoxDelShow.Checked == true)
+            {
+                flagYN = "";
+            }
+            else
+            {
+                flagYN = "AND flagYN = 'Y'";
+            }
+            Query = "select * from dawoon.dc_notemanage WHERE " + field + " like '%" + searchtext + "%' " + flagYN;
+            MySqlConnection con = new MySqlConnection(Connect);
+            MySqlCommand Comm  = new MySqlCommand(Query, con);
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = Comm;
+            DataTable dTable = new DataTable();
+            MyAdapter.Fill(dTable);
+            dataGridView1.DataSource = dTable;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[dataGridView1.Columns.Count - 4].Visible = false;
+            dataGridView1.Columns[dataGridView1.Columns.Count - 3].Visible = false;
+            dataGridView1.Columns[dataGridView1.Columns.Count - 2].Visible = false;
+            dataGridView1.Columns[dataGridView1.Columns.Count - 1].Visible = false;
+        }
+        private void clear()
+        {
+            textBoxName.Text = "";
+            textBoxSearch.Text = "";
+            richTextBoxMemo.Text = "";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex < 0)
+            {
+                return;
+            }
+            textBoxName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            richTextBoxMemo.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string seqstr = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString();
+                string Connect = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+                string Query = "UPDATE dawoon.dc_notemanage set flagYN='N' WHERE noteSeq=" + seqstr;
+                MySqlConnection con = new MySqlConnection(Connect);
+                MySqlCommand Comm = new MySqlCommand(Query, con);
+                MySqlDataReader Read;
+                con.Open();
+                Read = Comm.ExecuteReader();
+                MessageBox.Show("삭제완료");
+                con.Close();
+                buttonSearch_Click(sender, e);
+                clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string seqstr = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString();
+                string Connect = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+                string Query = "UPDATE dawoon.dc_notemanage SET noteSeq='" + seqstr + "',userNm"
+                    + textBoxName.Text + "',memo='" + richTextBoxMemo.Text + "; WHERE noteSeq='" + seqstr + "';";
+                MySqlConnection con = new MySqlConnection(Connect);
+                MySqlCommand Comm = new MySqlCommand(Query, con);
+                MySqlDataReader Read;
+                con.Open();
+                Read = Comm.ExecuteReader();
+                MessageBox.Show("수정완료");
+                con.Close();
+                buttonSave_Click(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxSearch.Text == "")
+            {
+                buttonSearch_Click(sender, e);
+            }
+        }
+
+        private void checkBoxDelShow_CheckedChanged(object sender, EventArgs e)
+        {
+            buttonSearch_Click(sender, e);
+        }
+    }
+}
